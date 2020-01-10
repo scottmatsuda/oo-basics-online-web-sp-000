0722e2dd9812c35be65f4047a362c666b17ff63e
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-10 08:36:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,8 +3,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
+    
   end
   
   def cobble
